743987a21302b16bb22e0ff2881de09c4b87b9f3
George Kontsevich <georgek@gmx.us>
2018-09-04 16:02:04 +0800

    BugFix: Added a Stack

--- startup_stm32f103xb.c
+++ startup_stm32f103xb.c
@@ -1,14 +1,16 @@
 
-void main(void);
-void __attribute__ ((weak, naked)) reset_handler(void) {
-    (*(volatile unsigned int *)(0x40021018)) |= (1 << 4);
+int main(void);
+  void __attribute__ ((weak, naked)) reset_handler(void) {
+      (*(volatile unsigned int *)(0x40021018)) |= (1 << 4);
 
-    (*(volatile unsigned int *)(0x40011004)) |= (0x00 << (((13 - 8) * 4) + 2));
-    (*(volatile unsigned int *)(0x40011004)) |= (0x02 << ((13 - 8) * 4));
-    main();
-}
+      (*(volatile unsigned int *)(0x40011004)) |= (0x00 << (((13 - 8) * 4) + 2));
+      (*(volatile unsigned int *)(0x40011004)) |= (0x02 << ((13 - 8) * 4));
+      main();
+  }
 
-
+void blocking_handler(void) { while (1); }
+void null_handler(void) {}
+extern unsigned _stack;
 
 __attribute__ ((section(".vectors")))
 struct {
@@ -27,5 +29,82 @@ struct {
     void (*systick)(void);
     void (*irq[68])(void);
 } vector_table = {
+    .initial_sp_value = &_stack,
     .reset = reset_handler,
+    .nmi = null_handler,
+    .hard_fault = blocking_handler,
+
+    .sv_call = null_handler,
+    .pend_sv = null_handler,
+    .systick = null_handler,
+    .irq = {
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+        null_handler,
+    }
 };
