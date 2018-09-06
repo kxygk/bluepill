c5d829c51549577ed231265bfc35ada064588aac
George Kontsevich <georgek@gmx.us>
2018-09-06 19:41:43 +0800

    Added: My own main.c

--- src/main.c
+++ src/main.c
@@ -1,10 +1,22 @@
 
-int main(void){
-    while(1) {
-        (*(volatile unsigned int *)(0x40011010)) = (1 << 13);
-        for (int i = 0; i < 1000000; ++i) __asm__("nop");
+#include "stm32f1xx_ll_bus.h"
+#include "stm32f1xx_ll_rcc.h"
+#include "stm32f1xx_ll_gpio.h"
 
-        (*(volatile unsigned short *)(0x40011014)) = (1 << 13);
-        for (int i = 0; i < 500000; ++i) __asm__("nop");
+int main(void)
+{
+
+LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOC);
+
+LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_13, LL_GPIO_MODE_OUTPUT);
+LL_GPIO_SetPinSpeed(GPIOC, LL_GPIO_PIN_13, LL_GPIO_SPEED_FREQ_HIGH);
+
+while (1)
+  {
+    LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_13);
+    int i = 200000;/* About 1/4 second delay */
+    while (i-- > 0) {
+        asm("nop");/* This stops it optimising code out */
     }
+  }
 }
