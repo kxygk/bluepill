ec099f96833fea918f44594a98321ed0c4a78efa
George Kontsevich <georgek@gmx.us>
2018-09-12 01:23:16 +0800

    Update: Use a smarter delay method using the internal clock

--- src/main.c
+++ src/main.c
@@ -61,11 +61,10 @@ LL_GPIO_SetPinSpeed(GPIOC, LL_GPIO_PIN_13, LL_GPIO_SPEED_FREQ_LOW);
 
 while (1)
   {
+    /* Insert delay 250 ms */
+    LL_mDelay(250);
     LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_13);
-    int i = 200000;/* About 1/4 second delay */
-    while (i-- > 0) {
-        asm("nop");/* This stops it optimising code out */
-    }
+
   }
 }
 
