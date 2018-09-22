d34b3ad81396a1533e176d46bbb15938a6bc38e7
George Kontsevich <georgek@gmx.us>
2018-09-22 17:37:07 +0800

    Added: SPI1 pin setup/configuration

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 
 #include "stm32f1xx_ll_system.h"
 #include "stm32f1xx_ll_utils.h"
+#include "stm32f1xx_ll_spi.h"
 
 void SystemClock_Config(void)
 {
@@ -48,6 +49,63 @@ void SystemClock_Config(void)
   /* Update CMSIS variable (which can be updated also through SystemCoreClockUpdate function) */
   LL_SetSystemCoreClock(72000000);
 }
+void SPI1_Init_Pins(void)
+{
+    // ** See Reference Manual RM0008 for details **
+    // SPI1 is on the APB2 peripheral (see section 3.1 of Reference Manual)
+    //
+    // Because there are a limited amount of pin on the chip
+    // SPI1 has to share pins with GPIO.
+    // There are two options available as listed in Section: 9.3.10
+    //
+    // We will use option 1 with shared pins on GPIOA (PA4-7)
+    // uint32_t NSS = LL_GPIO_PIN_4;
+    uint32_t SCK = LL_GPIO_PIN_5;
+    // uint32_t MISO = LL_GPIO_PIN_6;
+    uint32_t MOSI = LL_GPIO_PIN_7;
+    // First we enable the clock on GPIOA (ie. power it on)
+    LL_APB2_GRP1_EnableClock ( LL_APB2_GRP1_PERIPH_GPIOA );
+    // And enable the use of alternate functions on pins
+    LL_APB2_GRP1_EnableClock ( LL_APB2_GRP1_PERIPH_AFIO );
+
+    // TODO: Figure out what this is all about...
+    LL_GPIO_AF_Remap_SWJ_NOJTAG(); //??
+    while ( ( AFIO->MAPR & AFIO_MAPR_SWJ_CFG_JTAGDISABLE )  //??
+        != AFIO_MAPR_SWJ_CFG_JTAGDISABLE ); //??
+
+    // Next we need to tell the SPI1 pins to use their "alternate function"
+    LL_GPIO_AF_EnableRemap_SPI1();
+
+    LL_GPIO_SetPinMode ( GPIOA, SCK, LL_GPIO_MODE_ALTERNATE );
+    LL_GPIO_SetPinMode ( GPIOA, MOSI, LL_GPIO_MODE_ALTERNATE );
+
+    // And then set their speeds and default direction (pull down)
+    LL_GPIO_SetPinSpeed ( GPIOA, SCK, LL_GPIO_SPEED_FREQ_LOW );
+    LL_GPIO_SetPinPull ( GPIOA, SCK, LL_GPIO_PULL_DOWN );
+
+    LL_GPIO_SetPinSpeed ( GPIOA, MOSI, LL_GPIO_SPEED_FREQ_LOW );
+    LL_GPIO_SetPinPull ( GPIOA, MOSI, LL_GPIO_PULL_DOWN );
+
+    // Once all the pins are enabled correctly we can "power on" SPI1
+    LL_APB2_GRP1_EnableClock ( LL_APB2_GRP1_PERIPH_SPI1 );
+}
+
+void SPI1_Init(void)
+{
+    // SPI Parameters are collected into a struct
+    LL_SPI_InitTypeDef spi_initstruct;
+    spi_initstruct.BaudRate          = LL_SPI_BAUDRATEPRESCALER_DIV32;
+    spi_initstruct.TransferDirection = LL_SPI_HALF_DUPLEX_TX;
+    spi_initstruct.ClockPhase        = LL_SPI_PHASE_2EDGE;
+    spi_initstruct.ClockPolarity     = LL_SPI_POLARITY_HIGH;
+    spi_initstruct.BitOrder          = LL_SPI_MSB_FIRST;
+    spi_initstruct.DataWidth         = LL_SPI_DATAWIDTH_8BIT;
+    spi_initstruct.NSS               = LL_SPI_NSS_SOFT;
+    spi_initstruct.CRCCalculation    = LL_SPI_CRCCALCULATION_DISABLE;
+    spi_initstruct.Mode              = LL_SPI_MODE_MASTER;
+
+    LL_SPI_Init(SPI1, &spi_initstruct); // and sent into a Init function
+}
 
 #define BLINKY_PIN LL_GPIO_PIN_13
 #define TOGGLE_PIN LL_GPIO_PIN_14
@@ -55,6 +113,9 @@ void SystemClock_Config(void)
 int main(void)
 {
     SystemClock_Config();
+
+    SPI1_Init_Pins();
+    SPI1_Init();
 
     // enabling the clock on a peripheral effectively powers it on
     LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOC);
